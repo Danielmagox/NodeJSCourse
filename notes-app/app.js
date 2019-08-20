4764451d7694a7fc04b0fb85976d2abd5fa654c6
alu0100951615 <36208590+alu0100951615@users.noreply.github.com>
2019-08-21 00:41:19 +0100

    Validator Module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,7 @@
+const validator = require('validator')
 const {add,mul} = require('./utils.js')
 const cad = require('./notes.js')
 console.log(add(5,10))
 console.log(mul(5,10))
 console.log(cad());
+console.log(validator.isEmail('daniel130596@gmail.com'))
