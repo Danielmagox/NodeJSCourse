949706d7373caa3592524a573d35916a6bc3ba57
alu0100951615 <36208590+alu0100951615@users.noreply.github.com>
2019-08-21 01:01:42 +0100

    Chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,10 @@
 const validator = require('validator')
+const chalk = require('chalk');
 const {add,mul} = require('./utils.js')
 const cad = require('./notes.js')
+//Para subirlo a github mejor borrar la carpeta node_modules y despues hace npm install ya que la info la obtiene del package-lock.json
 console.log(add(5,10))
 console.log(mul(5,10))
 console.log(cad());
 console.log(validator.isEmail('daniel130596@gmail.com'))
+console.log(chalk.bold.green("Success") +  chalk.red(" ! "));
