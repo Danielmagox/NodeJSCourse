f2ab70ebf64bb6270635a0fc9c32b40b8e562188
alu0100951615 <36208590+alu0100951615@users.noreply.github.com>
2019-08-22 20:50:40 +0100

    Json explained and cleaned

--- playground/1-json.js
+++ playground/1-json.js
@@ -2,8 +2,9 @@ const fs= require('fs')
 
 //lees del archivo json
 //conviertes el buffer binario a un string util
-//cogemos ese String y lo parseamos como JSON 
+//cogemos ese String y lo parseamos como JSON para poder utilizar los datos de dentro
 //ya podemos acceder a los atributos
+// transformarlo nuevamente a String para imprimirlo
 
 var Buffer = fs.readFileSync('1-json.json')     
 var dataJSON = Buffer.toString()
@@ -11,10 +12,7 @@ var data = JSON.parse(dataJSON)
 data.name = "Danichi"
 data.age = "22"
 
-var planetJSON = JSON.stringify(data)  // transformarlo nuevamente a String
-
-console.log(data.age + " " +  data.name)
-
+var planetJSON = JSON.stringify(data)  
 fs.writeFileSync('2-json.json',planetJSON);
 
 
