3a314fe6e2705d439e4abd4f2291a56dd36335e4
alu0100951615 <36208590+alu0100951615@users.noreply.github.com>
2019-08-22 20:49:08 +0100

    Json explained

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,26 +1,11 @@
 const fs= require('fs')
 
-// const book = {
-//     title: 'Harry Potter',
-//     author: 'J.K Rowling'
-// }
+//lees del archivo json
+//conviertes el buffer binario a un string util
+//cogemos ese String y lo parseamos como JSON 
+//ya podemos acceder a los atributos
 
-// const bookJSON =  JSON.stringify(book)      // es un String
-// console.log(bookJSON)
-
-// const parseData = JSON.parse(bookJSON)      //Esto lo parsea y podemos pillar lo de dentro facilmente
-// console.log(parseData)
-// console.log(parseData.author)
-
-// fs.writeFileSync('1-json.json', bookJSON)   
-
-
-// const dataBuffer = fs.readFileSync('1-json.json')       //lees del archivo json
-// const dataJSON = dataBuffer.toString();                 //conviertes el buffer binario a un string util
-// const data = JSON.parse(dataJSON)                       //cogemos ese String y lo parseamos como JSON 
-// console.log(data.title)                                 //ya podemos acceder a los atributos
-
-var Buffer = fs.readFileSync('1-json.json')
+var Buffer = fs.readFileSync('1-json.json')     
 var dataJSON = Buffer.toString()
 var data = JSON.parse(dataJSON)
 data.name = "Danichi"
